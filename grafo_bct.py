39a2e441957dfce7e63eb449e824159a466a29ee
Débora Torre <deborinhaita@gmail.com>
2026-07-19 22:20:39 -0300

    Grafo BC&T

--- grafo_bct.py
+++ grafo_bct.py
@@ -3,9 +3,26 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-g = nx.Graph()
+g = nx.Graph([("BC&T", "Eng. Aero.",12), 
+               ("BC&T", "Química",11), 
+               ("BC&T", 'Biologia',8), 
+               ("BC&T", "Biotecnologia",11)])
 
-G.add_edge("BC&T", "Eng. Aero.", weight=12)
-G.add_edge("BC&T", "Química", weight=11)
-G.add_edge("BC&T", 'Biologia', weight=8)
-G.add_edge("BC&T", "Biotecnologia", weight=11)
+plt.figure(figsize=(20, 15))
+
+nx.draw_networkx(
+    g,
+    with_labels=True,
+    node_color="red",
+    node_size=800,
+    font_size=8,
+    arrows=True,
+    arrowsize=15,
+    edge_color='black',
+    alpha=0.5
+)
+
+plt.title("Grafo da relação entre o BC&T e os cursos específicos estudados", fontsize=16)
+plt.margins(0.1)
+plt.savefig("Imagens/grafo_bct_.png", dpi=300, bbox_inches='tight')
+plt.show()

2ef07a833d3fdcac9f97aed300b2ce3dca11ee00
Débora Torre <deborinhaita@gmail.com>
2026-07-19 22:08:53 -0300

    Fazendo o grafo do BC&T

---
+++ grafo_bct.py
@@ -0,0 +1,11 @@
+#Grafo BC&T
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+g = nx.Graph()
+
+G.add_edge("BC&T", "Eng. Aero.", weight=12)
+G.add_edge("BC&T", "Química", weight=11)
+G.add_edge("BC&T", 'Biologia', weight=8)
+G.add_edge("BC&T", "Biotecnologia", weight=11)
